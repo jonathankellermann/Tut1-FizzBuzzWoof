2ea6e61ff6afbbadfd0e105b12403d24019aa3fa
Jonathan Kellermann <jonathankellermann75@gmail.com>
2016-02-17 22:40:26 +0200

    Buzz and Comments Added

--- Tut1-FizzBuzzWoof/Hello.cpp
+++ Tut1-FizzBuzzWoof/Hello.cpp
@@ -1,25 +1,45 @@
 /*
 Jonathan Kellermann
 214502067
-FizzBuzzWoof
+Tut 1-FizzBuzzWoof
 */
 
 #include <iostream>
 
 using namespace std;
-
+//Main Loop
 int main()
 {
+	//Divisible Flag
+	bool isDivisible = false;
+	//Loop from 1 to 20 
 	for (int i = 1; i <= 20;i++)
 	{
+		//Fizz - Divisible by 3
 		if (i % 3 == 0)
 		{
-			cout << "Fizz" << endl;
+			//Print Fizz
+			cout << "Fizz ";
+		}
+		//Buzz - Divisible by 5
+		if (i % 5 == 0)
+		{
+			//Print Buzz
+			cout << "Buzz ";
+		}
+		//Divisible Flag
+		if (isDivisible==false)
+		{
+			//Print Number if not divisible by 3 or 5
+			cout << i << endl;
 		}
 		else
 		{
-			cout << i << endl;
+			//Break Line
+			cout << endl;
 		}
+		//Exit Loop
 		}
 	return 0;
 }
+//End
