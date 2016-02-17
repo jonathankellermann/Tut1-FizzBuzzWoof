ab73645dafd22d206b26b9b0fbe95c8ddbb58586
Jonathan Kellermann <jonathankellermann75@gmail.com>
2016-02-17 15:54:13 +0200

    main

---
+++ Tut1-FizzBuzzWoof/Hello.cpp
@@ -0,0 +1,17 @@
+/*
+Jonathan Kellermann
+214502067
+FizzBuzzWoof
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 20;i++)
+	{
+		cout << i + " " << end; 
+	}
+}
